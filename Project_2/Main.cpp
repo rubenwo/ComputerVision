cae8096ded09c6855fb393dfc54a5be1ec3355c4
Ruben <rwoldhui@avans.nl>
2019-05-08 17:35:11 +0200

    [Added] opencv blob detector

--- Project_2/Main.cpp
+++ Project_2/Main.cpp
@@ -1,86 +1,79 @@
-#include <iostream>
-#include "BlobDetection.h"
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include "opencv2/imgproc/imgproc.hpp" 
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/highgui.hpp"
 #include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
 
-using namespace cv;
-using namespace std;
-
-void detectBlobs(string);
-
-Mat labeledImage;
-Mat frame;
 
 int main(int argc, char* argv[])
 {
-	VideoCapture cap(0);
+	cv::SimpleBlobDetector::Params params;
+
+
+	//params.filterByArea = true;
+	//params.minArea = 1500;
+
+	//params.filterByCircularity = true;
+	//params.minCircularity = 0.8;
+	//params.maxCircularity = 1;
+
+
+	//params.filterByColor = true;
+	//params.blobColor = 0;
+	params.filterByConvexity = true;
+
+	params.minConvexity = 0.8;
+	params.maxConvexity = 1;
+
+	cv::VideoCapture cap;
+	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+
+	int deviceID = 0;
+	int apiID = cv::CAP_ANY;
+
+
+	cap.open(deviceID + apiID);
 
 	if (!cap.isOpened())
 	{
-		cout << "Cannot open the video cam" << endl;
+		std::cerr << "ERROR! Unable to open camera\n";
 		return -1;
 	}
 
-	//double dWidth = cap.get();
-	//double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
-	std::cout << "Hello world!";
-	namedWindow("Main");
-
-	while (true)
+	cv::Mat frame;
+	cv::Mat threshold_frame;
+	cv::Mat eroded_frame;
+	cv::Mat frame_with_key_points;
+	std::cout << "Start grabbing" << std::endl << "Press any key to terminate" << std::endl;
+	for (;;)
 	{
-		// Lees een nieuw frame
-		bool bSuccess = cap.read(frame);
+		cap.read(frame);
 
-		flip(frame, frame, ROTATE_180);
-
-		// Controlleer of het frame goed gelezen is.
-		if (!bSuccess)
+		if (frame.empty())
 		{
-			cout << "Cannot read a frame from video stream" << endl;
+			std::cerr << "ERROR! Blank frame grabbed\n";
 			break;
 		}
 
-		//BLOBdetection
-		detectBlobs("Main");
+		cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS,
+		                                            cv::Size(3, 3),
+		                                            cv::Point(0, 0));
 
-		// Het tonen van beeld
-		imshow("MyVideo", frame);
 
-		//  Wacht 30 ms op ESC-toets. Als ESC-toets is ingedrukt verlaat dan de loop
-		if (waitKey(1) == 27)
-		{
-			cout << "esc key is pressed by user" << endl;
-			break;
-		}
+		cv::threshold(frame, threshold_frame, 75, 150, cv::THRESH_BINARY);
+
+		cv::erode(threshold_frame, eroded_frame, element, cv::Point(-1, -1), 3);
+		std::vector<cv::KeyPoint> key_points;
+		detector->detect(eroded_frame, key_points);
+		drawKeypoints(frame, key_points, frame_with_key_points, cv::Scalar(0, 0, 255),
+		              cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+
+		//cv::imshow("Live", frame);
+		cv::imshow("KeyPoints", frame_with_key_points);
+		cv::waitKey(16);
 	}
 	return 0;
-}
-
-void detectBlobs(string targetWindow)
-{
-	Mat greyscale, binaryImage;
-	cvtColor(frame, greyscale, cv::COLOR_RGB2GRAY);
-	cv::threshold(greyscale, binaryImage, 128, ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY);
-
-	//BLOBdetection
-	Mat binary16S;
-	binaryImage.convertTo(binary16S, 3);
-	labelBLOBs(binary16S, labeledImage);
-	
-
-	std::vector<Point2d*> firstPicVector;
-	std::vector<Point2d*> posVec;
-	std::vector<int> areaVec;
-
-	int nBlobs = labelBLOBsInfo(binary16S, labeledImage, firstPicVector, posVec, areaVec, 500, 700);
-
-	cout << nBlobs << endl;
-
-	show16SImageStretch(labeledImage, targetWindow);
-	binary16S.release();
-	labeledImage.release();
 }
