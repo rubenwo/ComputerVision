26bd477b1fbff4b1387a893a2e987ce19f005453
Olaf van Wijngaarden <odwijnga@avans.nl>
2019-06-06 20:34:20 +0200

    [HALP] calculations for angles needed!

--- Project_4/Main.cpp
+++ Project_4/Main.cpp
@@ -6,9 +6,26 @@
 #include "FaceDetector.hpp"
 #include "SkinDetector.hpp"
 #include "BackgroundRemover.hpp"
+#include "../../../../../../../../../../opencv/build/include/opencv2/flann/dist.h"
+#include "../../../../../../../../../../opencv/build/include/opencv2/imgproc.hpp"
+#include "../../../../../../../../../../opencv/build/include/opencv2/core.hpp"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 cv::UMat frame, blurred, hsv, skin_mask;
 
+int GetAngleABC(cv::Point a, cv::Point b, cv::Point c)
+{
+	cv::Point ab = { b.x - a.x, b.y - a.y };
+	cv::Point cb = { b.x - c.x, b.y - c.y };
+
+	float dot = (ab.x * cb.x + ab.y * cb.y); // dot product
+	float cross = (ab.x * cb.y - ab.y * cb.x); // cross product
+
+	float alpha = atan2(cross, dot);
+
+	return (int)floor(alpha * 180. / M_PI + 0.5);
+}
 
 void drawContours(std::vector<cv::Mat>& contours, std::vector<cv::Vec4i>& hierarchy)
 {
@@ -35,13 +52,15 @@ void drawContours(std::vector<cv::Mat>& contours, std::vector<cv::Vec4i>& hierar
 	}
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) try
 {
 	cv::VideoCapture vcap("../resources/hand.mp4");
 	auto skin_lower = cv::Scalar(0, 0.23 * 255, 50);
 	auto skin_upper = cv::Scalar(50, 0.68 * 255, 255);
 
-	std::vector<cv::Mat> contours;
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Point> contourPoints;
+
 	std::vector<cv::Vec4i> hierarchy;
 
 	cv::useOpenVX();
@@ -55,6 +74,7 @@ int main(int argc, char* argv[])
 
 	for (;;)
 	{
+		int fingers = 0;
 		contours.clear();
 		hierarchy.clear();
 		if (vcap.read(frame))
@@ -73,15 +93,70 @@ int main(int argc, char* argv[])
 			cv::dilate(skin_mask, skin_mask, element, cv::Point(-1, -1), 4);
 			cv::erode(skin_mask, skin_mask, element, cv::Point(-1, -1), 2);
 
-
 			cv::findContours(skin_mask, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-			drawContours(contours, hierarchy);
+
+
+			int lrgctridx = 0;
+			int maxarea = 0;
+			for (int i = 0; i < contours.size(); i++)
+			{
+				double a = cv::contourArea(contours[i]);
+				if (a > maxarea)
+				{
+					maxarea = a;
+					lrgctridx = i;
+				}
+			}
+
+			std::vector<std::vector<cv::Point>> conHull(contours.size());
+			std::vector<std::vector<int> >inthull(contours.size());
+			std::vector<std::vector<cv::Vec4i>> defects(contours.size());
+			for (int i = 0; i < contours.size(); i++) { 
+				convexHull(contours[i], conHull[i]);
+				convexHull(contours[i], inthull[i]);
+				if (conHull[i].size() > 3)
+				{
+					convexityDefects(contours[i], inthull[i], defects[i]);
+				}
+			}
+
+			if (maxarea > 100) {
+				/// Draw convexityDefects
+				for (int j = 0; j < defects[lrgctridx].size(); ++j)
+				{
+					const cv::Vec4i& v = defects[lrgctridx][j];
+					float depth = v[3] / 256;
+
+					if (depth > 10) //  filter defects by depth
+					{
+						int startidx = v[0];
+						cv::Point2i ptStart(contours[lrgctridx][startidx]);
+						int endidx = v[1];
+						cv::Point ptEnd(contours[lrgctridx][endidx]);
+						int faridx = v[2];
+						cv::Point ptFar(contours[lrgctridx][faridx]);
+
+						int angle = GetAngleABC(ptStart, ptEnd, ptFar);
+
+						if (angle <= 90 && angle > 0) //only draw fingers that are not folded
+						{
+							fingers++;
+							line(frame, ptStart, ptFar, cv::Scalar(255, 0, 0), 1);
+							line(frame, ptEnd, ptFar, cv::Scalar(0, 0, 255), 1);
+							circle(frame, ptFar, 4, cv::Scalar(0, 255, 0), 2);
+						}
+					}
+				}
+			}
+
+			// drawContours(contours, hierarchy); //Not needed to test
+
 			//-----Show results-----//
 			cv::imshow("Original", frame);
-			cv::imshow("Blurred", blurred);
-			cv::imshow("HSV", hsv);
-			cv::imshow("Skin Mask", skin_mask);
+			// cv::imshow("Blurred", blurred);
+			// cv::imshow("HSV", hsv);
+			// cv::imshow("Skin Mask", skin_mask);
 		}
 		else
 		{
@@ -93,3 +168,8 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+catch (cv::Exception e)
+{
+	std::cout << "An exception occurred. Exception Nr. " << e.err << '\n';
+}
+
